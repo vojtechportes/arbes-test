7980f60aef731012236c80c98c9af6f010be376b
Vojtech Portes <vojtech.portes@signals.network>
2019-03-29 11:58:02 +0100

    Homepage update

--- src/client/scenes/Homepage/index.tsx
+++ src/client/scenes/Homepage/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { StringsContext } from "src/client/strings/index";
 import { Typography, Button } from "@material-ui/core";
-import { Container } from "src/client/components/index";
+import { Container, Margin } from "src/client/components/index";
 
 class Homepage extends React.PureComponent<RouteComponentProps> {
   public render() {
@@ -18,6 +18,22 @@ class Homepage extends React.PureComponent<RouteComponentProps> {
               <Typography variant="h4" color="inherit">
                 {homepage.hd}
               </Typography>
+              <Margin margin="20px 0">
+                <Typography variant="body1">
+                  or sit amet, consectetuer adipiscing elit. Curabitur sagittis
+                  hendrerit ante. Pellentesque arcu. Fusce dui leo, imperdiet
+                  in, aliquam sit amet, feugiat eu, orci. Nemo enim ipsam
+                  voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+                  sed quia consequuntur magni dolores eos qui ratione voluptatem
+                  sequi nesciunt. Praesent id justo in neque elementum ultrices.
+                  Cras pede libero, dapibus nec, pretium sit amet, tempor quis.
+                  Sed convallis magna eu sem. Vivamus luctus egestas leo. Fusce
+                  nibh. Duis condimentum augue id magna semper rutrum. Aenean id
+                  metus id velit ullamcorper pulvinar. Donec quis nibh at felis
+                  congue commodo. Maecenas sollicitudin. Integer imperdiet
+                  lectus quis justo.
+                </Typography>
+              </Margin>
               <Button
                 onClick={this.handleAddContactClick}
                 variant="contained"
